36b719b0ff4cf16ebb845d06a49876d751b280f3
SamanKeighobadi <saman.keighobadi@gmail.com>
2025-09-08 17:53:05 +0330

    feat: create BookCard component , define daisyUI colors & sizes class name

---
+++ client/src/components/books/BookCard.vue
@@ -0,0 +1,33 @@
+<script lang="ts" setup>
+import BaseButton from '@/base/BaseButton.vue';
+
+
+interface BookCardProps {
+    image:string;
+    title: string;
+    publisher: string;
+    author: string;
+}
+
+const props = defineProps<BookCardProps>()
+
+</script>
+
+
+<template>
+
+    <div class="card bg-base-100 w-80 shadow-md">
+        <figure>
+            <img :src="image"
+                :alt="props.title" />
+        </figure>
+        <div class="card-body">
+            <h2 class="card-title">{{ props.title }}</h2>
+            <p>{{ props.publisher }}</p>
+            <p>{{ props.author }}</p>
+            <div class="card-actions justify-end">
+                <BaseButton color="neutral">مشاهده</BaseButton>
+            </div>
+        </div>
+    </div>
+</template>
---
+++ client/src/types/ui.types.ts
@@ -0,0 +1,14 @@
+export type DaisyUIColors =
+  | "neutral"
+  | "primary"
+  | "info"
+  | "error"
+  | "secondary"
+  | "accent"
+  | "success";
+export type DaisyUISizes = "xs" | "sm" | "md" | "lg";
+
+export interface SelectOption {
+  label: string | number;
+  value: string | number;
+}
