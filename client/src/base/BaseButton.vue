724b819606f7c58d5fa066c4363fb69cb6008c2d
SamanKeighobadi <saman.keighobadi@gmail.com>
2025-09-08 17:51:53 +0330

    feat: create re-usable , ui components BaseButton & BaseInput,
    - install daisyui & initilize theme

---
+++ client/src/base/BaseButton.vue
@@ -0,0 +1,32 @@
+<script lang="ts" setup>
+import type { DaisyUIColors, DaisyUISizes } from '@/types/ui.types';
+
+
+interface BaseButtonProps {
+    color: DaisyUIColors,
+    disabled?: boolean,
+    size?: DaisyUISizes,
+    loading?: boolean
+}
+
+const { color, disabled, loading, size } = defineProps<BaseButtonProps>()
+
+
+const buttonClasses = [
+    'btn',
+    [`btn-${color}`],
+    [`btn-${size || "md"}`],
+    disabled || loading ? 'opacity-50 cursor-not-allowed' : '',
+    loading ? 'relative' : ''
+].filter(Boolean).join(' ')
+
+
+
+</script>
+
+<template>
+
+    <button :class="buttonClasses" :disabled="disabled || loading">
+        <slot></slot>
+    </button>
+</template>
---
+++ client/src/base/BaseInput.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+
+import type { DaisyUIColors, DaisyUISizes } from '@/types/ui.types';
+type InputType = "text" | "password" | "email"
+
+interface InputProps {
+    color: DaisyUIColors;
+    size?: DaisyUISizes;
+    type?: InputType;
+    placeholder?: string
+    disabled?: boolean
+}
+
+const { color, placeholder, size, type } = defineProps<InputProps>()
+const model = defineModel<string>({ default: '' })
+
+
+const inputClasses = [
+    "input",
+    [`input-${color}`],
+    [`input-${size}`]
+].filter(Boolean).join(' ')
+
+</script>
+
+<template>
+
+    <input :type="type" :placeholder="placeholder" :class="inputClasses" :disabled="disabled" v-model="model">
+</template>
